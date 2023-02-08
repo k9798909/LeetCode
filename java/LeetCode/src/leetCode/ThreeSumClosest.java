ec9b8c490805bb0d6bd2ca4ba45bcd903d36ab82
k9798909 <k9798909@gmail.com>
2023-02-08 16:45:07 +0800

    ThreeSumClosest

---
+++ java/LeetCode/src/leetCode/ThreeSumClosest.java
@@ -0,0 +1,58 @@
+package leetCode;
+
+import java.util.Arrays;
+
+public class ThreeSumClosest {
+    public static int threeSumClosest(int[] nums, int target) {
+    	int num = nums[0] + nums[1] + nums[2];
+    	for (int i = 0; i < nums.length - 2 ; i++) {
+			for (int j = i + 1; j < nums.length - 1; j++) {
+				for (int k = j + 1; k < nums.length; k++) {
+					int temp = nums[i] + nums[j] + nums[k];
+					if (temp == target) {
+						return temp;
+					}
+					
+					if (Math.abs(num - target) > Math.abs(temp - target)) {
+						num = temp;
+					}
+				}
+			}
+		}
+        return num;
+    }
+    
+    public static int threeSumClosest2(int[] nums, int target) {
+    	Arrays.sort(nums);
+    	int num = nums[0] + nums[1] + nums[2];
+    	
+		for (int i = 0; i < nums.length - 2; i++) {
+	    	int j = i + 1;
+	    	int k = nums.length - 1;
+	    	
+	    	while(k > j) {
+				int temp = nums[i] + nums[j] + nums[k];
+				if (temp == target) {
+					return temp;
+				}
+
+				if (Math.abs(num - target) > Math.abs(temp - target)) {
+					num = temp;
+				}
+				
+				if (temp > target) {
+					k--;
+				} else if (temp < target) {
+					j++;
+				}
+	    	}
+			
+		}
+        return num;
+    }
+    
+	public static void main(String[] args) {
+		System.out.println(threeSumClosest2(new int[] { 4,0,5,-5,3,3,0,-4,-5 }, -2));
+	}
+    
+}
